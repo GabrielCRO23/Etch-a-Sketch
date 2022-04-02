d08320478899242e2aed21c86a716b6e3ac2c2cd
Gabriel Hrastovic <gabrielcro2369@gmail.com>
2022-04-02 02:54:34 -0400

    --all

--- javascript.js
+++ javascript.js
@@ -1,24 +1,62 @@
-const container = document.querySelector('#container')
-for (let i = 0; i < 256; i++) {
-    container.innerHTML += '<div class="box"></div>';
- }
-const boxes = document.querySelectorAll('.box');
+const gridContainer = document.querySelector("#gridContainer");
+const resetButton = document.querySelector('#resetButton');
+const slider = document.querySelector("#myRange")
 
+    function createGrid() {
+    for (i = 0; i < 256; i++) {
+        gridContainer.innerHTML += '<div class = "box"></div>'
+    }
+    }; 
 
+    createGrid();
 
- boxes.forEach((box) => { 
+    const boxes = document.querySelectorAll(".box")
+    boxes.forEach((box) => { 
 
-function getEffect() {
-     box.style.backgroundColor = "red";
-}
+            function getEffect() {
+            box.style.backgroundColor = getRandomColor();
+        };
 
+            box.addEventListener('mouseover', function() {
+            getEffect();
+        })
+        });
 
+            function removeAllChildNodes(parent) {
+            while(parent.firstChild) {
+                parent.removeChild(parent.firstChild);
+            }
+        };
 
+        
 
-
-
-box.addEventListener('mouseover', function() {
-    getEffect();
-})
-
+    slider.addEventListener('input', function() {
+    let val = document.getElementById('myRange').value;
+    removeAllChildNodes(gridContainer);
+    gridContainer.setAttribute('style', `grid-template-columns: repeat(${val}, 2fr); grid-template-rows: repeat(${val}, 2fr);`);
+    for (let i = 0; i < val*val; i++) {
+        const div = document.createElement('div');
+        div.classList.add('box');
+        div.addEventListener('mouseover', function(event){
+            event.target.style.backgroundColor = getRandomColor();
+        });
+        gridContainer.appendChild(div); 
+    };
 });
+    
+resetButton.addEventListener('click', function(){
+    let val = document.getElementById('myRange').value;
+    let box = gridContainer.children;
+    for (let i = 0; i < val * val; i++) {
+        box[i].style.backgroundColor = 'white';
+    }
+});
+        
+function getRandomColor(){
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+}
